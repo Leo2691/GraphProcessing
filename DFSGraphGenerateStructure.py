ce4980e97dcdcc41454d324b6b7cebf827e16007
Leo2691 <levantonov@yandex.ru>
2019-09-02 18:01:43 +0300

    Update alg

--- DFSGraphGenerateStructure.py
+++ DFSGraphGenerateStructure.py
@@ -2,43 +2,63 @@ import numpy as np
 import collections
 
 class Graph():
-    def __init__(self, vertices, start, end):
+    def __init__(self, vertices, start, end, limitCon):
         self.graph = collections.defaultdict(list) 
         self.V = vertices 
         self.start = start
         self.end = end
+        self.limitCon = limitCon
 
     def addEdge(self, u, v): 
         self.graph[u].append(v) 
 
 
     def generateStruct(self):
-        visited = [0] * self.V 
-        recStack = [0] * self.V 
+        visited = np.array([0] * self.V)
+        elementsMask = [True] * self.V 
+        elementsMask[self.start] = False 
 
         for i in np.arange(1, 100):
             # count connections with input
-            countCon = np.random.randint(1, 4)
+            countCon = np.random.randint(1, self.limitCon + 1)
             # ids connections with input
-            ids = np.unique([np.random.randint(3, self.V+1) for i in np.arange(countCon)]) 
+            ids = np.unique([np.random.randint(self.limitCon, self.V+1) for i in np.arange(countCon)]) 
 
             for j in ids:
                  # add conns with input 
-                g.addEdge(start, j)
-                if visited[j] == 0:
-                    self.genCon(j, visited, recStack)
+                g.addEdge(self.start, j)
+                if visited[j] <= self.limitCon:
+                    self.genCon(j, visited, elementsMask)
 
-    def genCon(self, v, visited, recStack):
-        visited[v] = 1
-        recStack[v] = 1
+    def genCon(self, v, visited, elementsMask):
+        visited[v] += 1 
+        #elementsMask[v] = False
+
+         # count connections with input
+        countCon = np.random.randint(1, self.limitCon + 1)
+        # list elements for chousen
+        posiblElems = np.arange(self.V)[elementsMask]
+        # ids connections with input
+        ids = np.unique([np.random.choice(posiblElems) for i in np.arange(countCon)]) 
         
 
 
 
         return 1
                 
+t = [False] * 8
+a = np.zeros(8)
+
+t[1] = True
+t[3] = True
+
+b = a[t]
 
 
-g = Graph(8, 1, 2)
+r = np.arange(8)
+
+g = Graph(8, 0, 1, 3)
 
 g.generateStruct()
+
+
